688c37898ffab5501deb955451d4b0c60a4ff1ee
Arun Motoori <arun.motoori@gmail.com>
2018-10-23 17:17:24 +0530

    Modified the framework a bit and started automating the Register feature file

--- src/main/java/com/tutorialsninja/automation/base/Base.java
+++ src/main/java/com/tutorialsninja/automation/base/Base.java
@@ -21,28 +21,7 @@ public class Base {
 	public static WebDriver driver;
 	public static ConfigurationReader reader;
 	
-	
-	
-	
-	
-	@Before
-	public void setUp(Scenario scenario){
-		log.info("Scenario Started: "+scenario.getName());
-		PropertyConfigurator.configure(PathHelper.getResourcePath("/src/main/resources/ConfigurationFile/log4j.properties"));
-		reader=new PropertyFileReader();
-		Browser.startBrowser();
-		Browser.maximize();
-	}
-	
-	@After
-	public void closeBrowser(Scenario scenario){
-		if(scenario.isFailed()){
-			scenario.embed(Browser.takeScreenshot(), "image/png");
-		}
-		log.info("Scenario Completed: "+scenario.getName());
-		log.info("Scenario Status is: "+scenario.getStatus());
-		Base.driver.quit();
-		}
+
 	}
 
 
