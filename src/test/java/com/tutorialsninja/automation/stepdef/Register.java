178d3b0852e46a659fdaf376a91dd9bcad950bc3
Arun Motoori <arun.motoori@gmail.com>
2018-10-25 17:01:08 +0530

    Completed automating all the scenarios in the Register feature file

--- src/test/java/com/tutorialsninja/automation/stepdef/Register.java
+++ src/test/java/com/tutorialsninja/automation/stepdef/Register.java
@@ -1,11 +1,24 @@
 package com.tutorialsninja.automation.stepdef;
 
+import org.junit.Assert;
+
 import com.tutorialsninja.automation.base.Base;
+import com.tutorialsninja.automation.framework.Elements;
+import com.tutorialsninja.automation.pages.AccountSuccessPage;
+import com.tutorialsninja.automation.pages.HeadersPage;
+import com.tutorialsninja.automation.pages.RegisterPage;
 
 import cucumber.api.DataTable;
-import cucumber.api.java.en.*;
+import cucumber.api.java.en.And;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
 
 public class Register {
+	
+	HeadersPage headersPage = new HeadersPage();
+	RegisterPage registerPage = new RegisterPage();
+	AccountSuccessPage accountSuccessPage = new AccountSuccessPage();
 	
 	@Given("^I launch the application$")
 	public void i_launch_the_application() {
@@ -16,29 +29,77 @@ public class Register {
 
 	@And("^I navigate to Account Registration page$")
 	public void i_navigate_to_Account_Registration_page()  {
+		
+		Elements.click(HeadersPage.myAccountLink);
+		Elements.click(HeadersPage.register);
 	    
 	}
 
 	@When("^I provide all the below valid details$")
 	public void i_provide_all_the_below_valid_details(DataTable dataTable) {
 		
-		
+		RegisterPage.enterAllRegistrationDetails(dataTable,"unique");
 	   
 	}
 
 	@And("^I select the Privacy Policy$")
 	public void i_select_the_Privacy_Policy() {
 	  
+		Elements.click(RegisterPage.privacyPolicy);
 	}
 
 	@And("^I click on Continue button$")
 	public void i_click_on_Continue_button() {
+		
+		Elements.click(RegisterPage.continueButton);
 	    
 	}
 
 	@Then("^I should see that the User Account has successfully created$")
 	public void i_should_see_that_the_User_Account_has_successfully_created(){
+		
+		Assert.assertTrue(Elements.isDisplayed(AccountSuccessPage.successBreadcrumb));
 	    
+	}
+	
+	@Then("^I should see that the User Account is not created$")
+	public void i_should_see_that_the_User_Account_is_not_created() {
+	   
+		Assert.assertTrue(Elements.isDisplayed(RegisterPage.registerBreadcrumb));
+		
+	}
+
+	@Then("^I should see the error messages informing the user to fill the mandatory fields$")
+	public void i_should_see_the_error_messages_informing_the_user_to_fill_the_mandatory_fields() {
+	    
+		Assert.assertTrue(Elements.isDisplayed(RegisterPage.firstNameWarning));
+		Assert.assertTrue(Elements.isDisplayed(RegisterPage.lastNameWarning));
+		Assert.assertTrue(Elements.isDisplayed(RegisterPage.emailWarning));
+		Assert.assertTrue(Elements.isDisplayed(RegisterPage.telephoneWarning));
+		Assert.assertTrue(Elements.isDisplayed(RegisterPage.passwordWarning));
+		Assert.assertTrue(Elements.isDisplayed(RegisterPage.mainWarning));
+		
+	}
+	
+	@When("^I subscribe to Newsletter$")
+	public void i_subscribe_to_Newsletter()  {
+		
+		Elements.click(RegisterPage.yetToNewsletter);
+	    
+	}
+	
+	@When("^I provide the below duplicate details into the fields$")
+	public void i_provide_the_below_duplicate_details_into_the_fields(DataTable dataTable){
+		
+		RegisterPage.enterAllRegistrationDetails(dataTable,"duplicate");
+	    
+	}
+
+	@Then("^I should see the warning message stating that the user is already created$")
+	public void i_should_see_the_warning_message_stating_that_the_user_is_already_created() {
+	    
+		Assert.assertTrue(Elements.VerifyTextEquals(RegisterPage.mainWarning,"Warning: E-Mail Address is already registered!"));
+		
 	}
 	
 
